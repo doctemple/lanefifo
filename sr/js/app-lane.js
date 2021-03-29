df3a9201fe49d2bbc95f48e491fbebaea7283812
siriwat <siriwat.chanakhun@gmail.com>
2021-03-29 11:41:00 +0700

    Fist Commit

---
+++ sr/js/app-lane.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function ($scope, $http, $interval,$timeout,$window) {
+  // API URL
+  $scope.api_json = "http://192.168.20.248:88/api/lanes/"+lane;
+  //$scope.api_json = "api/lene.json";
+
+  $scope.system_name = "FIFO";
+
+  $scope.interval = 1000;
+
+  $interval(function () {
+    $http.get($scope.api_json).then(function (response) {
+      $scope.sensor = response.data;
+      $scope.minutes = $scope.sensor.time.substring(0,2);
+      $scope.seconds = $scope.sensor.time.substring(3,5);
+    });
+  }, $scope.interval);
+
+
+});
